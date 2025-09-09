a1b484ef4f0dcd1fe974c3a1aa97025f7f4461a5
Abhijith Balagurusamy <abhibalagurusamy@gmail.com>
2025-09-09 09:57:20 -0400

    fixed the formatting issues by running tf check locally

--- infrastructure/terraform/apigw.tf
+++ infrastructure/terraform/apigw.tf
@@ -1,41 +1,41 @@
 resource "aws_apigatewayv2_api" "http_api" {
-  name = local.api_name
+  name          = local.api_name
   protocol_type = "HTTP"
 }
 
 resource "aws_apigatewayv2_vpc_link" "this" {
-  name = local.vpc_link_name
-  subnet_ids = data.aws_subnets.default.ids
+  name               = local.vpc_link_name
+  subnet_ids         = data.aws_subnets.default.ids
   security_group_ids = [aws_security_group.vpc_link.id]
 }
 
 resource "aws_apigatewayv2_integration" "nlb_proxy" {
-  api_id = aws_apigatewayv2_api.http_api.id
-  integration_type = "HTTP_PROXY"
-  connection_type = "VPC_LINK"
-  connection_id = aws_apigatewayv2_vpc_link.this.id
-  integration_method = "ANY"
+  api_id                 = aws_apigatewayv2_api.http_api.id
+  integration_type       = "HTTP_PROXY"
+  connection_type        = "VPC_LINK"
+  connection_id          = aws_apigatewayv2_vpc_link.this.id
+  integration_method     = "ANY"
   payload_format_version = "1.0"
   # For HTTP API + VPC Link, integration_uri must be the ALB/NLB listener ARN
-  integration_uri = aws_lb_listener.http.arn
+  integration_uri      = aws_lb_listener.http.arn
   timeout_milliseconds = 30000
 }
 
 resource "aws_apigatewayv2_route" "root" {
-  api_id = aws_apigatewayv2_api.http_api.id
+  api_id    = aws_apigatewayv2_api.http_api.id
   route_key = "ANY /"
-  target = "integrations/${aws_apigatewayv2_integration.nlb_proxy.id}"
+  target    = "integrations/${aws_apigatewayv2_integration.nlb_proxy.id}"
 }
 
 resource "aws_apigatewayv2_route" "proxy" {
-  api_id = aws_apigatewayv2_api.http_api.id
+  api_id    = aws_apigatewayv2_api.http_api.id
   route_key = "ANY /{proxy+}"
-  target = "integrations/${aws_apigatewayv2_integration.nlb_proxy.id}"
+  target    = "integrations/${aws_apigatewayv2_integration.nlb_proxy.id}"
 }
 
 resource "aws_apigatewayv2_stage" "prod" {
-  api_id = aws_apigatewayv2_api.http_api.id
-  name = "prod"
+  api_id      = aws_apigatewayv2_api.http_api.id
+  name        = "prod"
   auto_deploy = true
 }
 
